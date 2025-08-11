a26ed74344402104f94ac9e746a0efd7e1024618
zakymzn <mamurzakynurrokhman@gmail.com>
2025-08-11 15:39:47 +0700

    codelab firebase codelab messaging (fcm)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
